9527c476fa3ca606330fccd033fa85fae605527f
Mohammad Hroub <mohammad.hroub.1991@gmail.com>
2020-01-07 12:38:49 +0200

    add tests

---
+++ src/test/Test.java
@@ -0,0 +1,28 @@
+
+public class Test extends TestCase {
+
+    public void testPaperBeatsRock() {
+        Paper paper = new Paper();
+        Rock rock = new Rock();
+        assertTrue(paper.beats(rock) > 0);
+        assertFalse(rock.beats(paper) >= 0);
+
+    }
+
+    public void testPaperTiesPaper() {
+        Paper paper1 = new Paper();
+        Paper paper2 = new Paper();
+        assertTrue(paper1.beats(paper2) == 0);
+        assertFalse(paper1.beats(paper2) > 0);
+        assertFalse(paper1.beats(paper2) < 0)
+    }
+
+    public void testScissorBeatsPaper() {
+        Scissor scissor = new Scissor();
+        Paper paper = new Paper();
+        assertTrue(scissor.beats(paper) >0);
+        assertFalse(paper.beats(scissor) >= 0);
+    }
+
+
+}
